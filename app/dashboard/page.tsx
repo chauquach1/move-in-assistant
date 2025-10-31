eccd0410bd029904b7663f5b9f08a9f3da121de6
chauquach1 <chau268@gmail.com>
2025-10-31 16:21:36 -0700

    initial development commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,31 +1,10 @@
-import {Input} from '@heroui/input';
-import {Button, ButtonGroup} from "@heroui/button";
+"use client";
 
 export default function Dashboard() {
-  const sizes = ["sm", "md", "lg"] as const;
-
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans ">
-      <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <div className="flex w-full gap-4">
-          <div className="w-full border p-4 rounded-lg">
-            <h1 className="underline mb-4">Unit/Property Details</h1>
-            <Input 
-              name="unitId"
-              label="Unit Id"
-              placeholder="Enter Unit Display ID"
-              className="mb-4"
-            />
-            <Input name="unitShortname" label="City" placeholder="Enter city" className="mb-4" />
-            <Input name="propertyAddress" label="Property Address" placeholder="Enter property address" className="mb-4" />
-
-            <Button
-              color='primary'
-            >
-              Button
-            </Button>
-          </div>
-        </div>
+    <div className="flex min-h-screen items-start justify-center bg-content1/30 font-sans">
+      <main className="mx-auto grid w-full max-w-7xl grid-cols-1 gap-6 p-6 md:grid-cols-2 xl:grid-cols-3">
+        
       </main>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,9 @@
-// import { Input } from "@heroui/input";
-// import { Calendar } from "@heroui/calendar";
 
 export default function Home() {
-  const colors = ["default", "primary", "secondary", "success", "warning", "danger"] as const;
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        {/* <div className="w-full flex flex-row flex-wrap gap-4">
-          {colors.map((color) => (
-            <Input
-              key={color}
-              className="max-w-[220px]"
-              color={color}
-              defaultValue="junior@heroui.com"
-              label="Email"
-              placeholder="Enter your email"
-              type="email"
-            />
-          ))}
-        </div>  
-        <Calendar className="mt-16" /> */}
+        
       </main>
     </div>
   );
