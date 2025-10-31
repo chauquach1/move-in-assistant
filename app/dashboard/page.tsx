302f9bd7835d9951e7251b68558a0006a9261ee6
chauquach1 <chau268@gmail.com>
2025-10-31 16:21:36 -0700

    initial development commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,8 +3,6 @@ import {Button, ButtonGroup} from "@heroui/button";
 import {LeaseSummaryForm} from "../../components/LeaseSummaryForm";
 
 export default function Dashboard() {
-  const sizes = ["sm", "md", "lg"] as const;
-
   return (
     <div className="flex flex-col min-h-screen items-center justify-start font-sans ">
       <main className=" h-full w-full flex-col items-center justify-between p-4 bg-gray-500 sm:items-start">
