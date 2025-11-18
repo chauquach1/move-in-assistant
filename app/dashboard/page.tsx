108204931d27596b9d3f249ab4abcbc4f14e36e7
chauquach1 <chau268@gmail.com>
2025-11-17 21:36:06 -0800

    updated dashboard/page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
-"use client";
+import {Input} from '@heroui/input';
+import {Button, ButtonGroup} from "@heroui/button";
+import {LeaseSummaryForm} from "../../components/LeaseSummaryForm";
 
 export default function Dashboard() {
   return (
-    <div className="flex min-h-screen items-start justify-center bg-content1/30 font-sans">
-      <main className="mx-auto grid w-full max-w-7xl grid-cols-1 gap-6 p-6 md:grid-cols-2 xl:grid-cols-3">
-        
+    <div className="flex flex-col min-h-screen items-center justify-start font-sans ">
+      <main className=" h-full w-full flex-col items-center justify-between p-4 bg-gray-500 sm:items-start">
+        <LeaseSummaryForm/>
       </main>
     </div>
   );
