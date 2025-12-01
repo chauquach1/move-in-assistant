b43e4623b46bf8989a392bdf7699fb4bcb0b7313
chauquach1 <chau268@gmail.com>
2025-12-01 15:17:05 -0800

    created HOA.tsx form section

---
+++ components/(form)/(form-sections)/HOA.tsx
@@ -0,0 +1,45 @@
+'use client'
+import { useState } from "react";
+import { SubjectContainer } from "@/components/(containers)/SubjectContainer";
+import { cn } from "@heroui/react";
+import {Switch} from "@heroui/switch";
+import { TextInput } from "../(inputs)/TextInput";
+
+export const HOA = () => {
+    const [tenants, setTenants] = useState<string[]>([""]);
+    const [guarantors, setGuarantors] = useState<string[]>([""]);
+
+  return (
+    <SubjectContainer title="HOA">
+      <div className="flex flex-col gap-2">
+        <div className="inline-flex gap-2">
+          <Switch
+            name="hoaExists"
+            classNames={{
+              base: cn("inline-flex flex-row-reverse gap-2"),
+            }}
+          >
+            <p>HOA?</p>
+          </Switch>
+          <TextInput subject="HOA Name" labelPlacement="inside" variant="flat" className="border-none"/>
+        </div>
+        <Switch
+          name="hoaParkingPass"
+          classNames={{
+            base: cn("inline-flex flex-row-reverse gap-2"),
+          }}
+        >
+          <p>Parking Pass?</p>
+        </Switch>
+        <Switch
+          name="hoaRulesProvided"
+          classNames={{
+            base: cn("inline-flex flex-row-reverse gap-2"),
+          }}
+        >
+          <p>Rules/Regulations Provided?</p>
+        </Switch>
+      </div>
+    </SubjectContainer>
+  );
+}
--- components/LeaseSummaryForm.tsx
+++ components/LeaseSummaryForm.tsx
@@ -2,15 +2,17 @@ import { Form } from "@heroui/form";
 import { LeaseTermDetails } from "./(form)/(form-sections)/LeaseTermDetails";
 import { RentAmounts } from "./(form)/(form-sections)/RentAmounts";
 import { Landscaping } from "./(form)/(form-sections)/Landscaping";
+import { HOA } from "./(form)/(form-sections)/HOA";
 
 export const LeaseSummaryForm = ({...props }) => {
   return (
     <Form id="lease-summary-form" {...props}>
       <h1 className="text-2xl font-bold mb-4">Lease Summary Form</h1>
-      <div className="container flex flex-row justify-center min-w-full min-h-full gap-2">
+      <div className="container flex flex-row flex-wrap justify-center min-w-full min-h-full gap-2">
         <LeaseTermDetails />
         <RentAmounts />
         <Landscaping />
+        <HOA />
       </div>
     </Form>
   );
