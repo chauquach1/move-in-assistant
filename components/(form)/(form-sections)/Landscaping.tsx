09e95ca8058b150fc995d0f7647f82cdff6286d8
chauquach1 <chau268@gmail.com>
2025-12-05 15:38:05 -0800

    removed unused useState statements and matched defaultValue to field names

--- components/(form)/(form-sections)/Landscaping.tsx
+++ components/(form)/(form-sections)/Landscaping.tsx
@@ -1,20 +1,16 @@
-'use client'
-import { useState } from "react";
 import { SubjectContainer } from "@/components/(containers)/SubjectContainer";
 import {RadioGroup, Radio} from "@heroui/radio";
 
 export const Landscaping = () => {
-    const [tenants, setTenants] = useState<string[]>([""]);
-    const [guarantors, setGuarantors] = useState<string[]>([""]);
 
   return (
     <SubjectContainer title="Landscaping">
       <div className="grid grid-cols-1 grid-rows-2 col-span-2 gap-4">
-        <RadioGroup name="landscapingWatering" label="Watering" defaultValue="Landlord" >
+        <RadioGroup name="landscapingWatering" label="Watering" defaultValue="landlord" >
           <Radio value="landlord">Landlord</Radio>
           <Radio value="tenant">Tenant</Radio>
         </RadioGroup>
-        <RadioGroup name="landscapingMaintenance" label="Maintenance" defaultValue="Landlord" >
+        <RadioGroup name="landscapingMaintenance" label="Maintenance" defaultValue="landlord" >
           <Radio value="landlord">Landlord</Radio>
           <Radio value="tenant">Tenant</Radio>
         </RadioGroup>
