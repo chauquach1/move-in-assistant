376b1036f5550a58f12feea4d0ab2eaad4268832
chauquach1 <chau268@gmail.com>
2025-12-05 15:05:52 -0800

    updated names for the following fields: ProratedRentDueDate, Storage#, ParkingSpot#

--- components/(form)/(form-sections)/RentAmounts.tsx
+++ components/(form)/(form-sections)/RentAmounts.tsx
@@ -12,14 +12,26 @@ export const RentAmounts = () => {
       <div className="flex flex-col col-span-2 gap-3">
         <MoneyInput subject="Base Rent" />
         <MoneyInput subject="Parking Rent" />
-        <TextInput subject="Parking Spot #" placeholder="N/A" labelPlacement="outside-top" defaultValue="N/A" />
+        <TextInput
+          subject="Parking Spot #"
+          actualName="parkingLocation"
+          placeholder="N/A"
+          labelPlacement="outside-top"
+          defaultValue="N/A"
+        />
         <MoneyInput subject="Storage Rent" />
-        <TextInput subject="Storage #" placeholder="N/A" labelPlacement="outside-top" defaultValue="N/A" />
+        <TextInput
+          subject="Storage #"
+          actualName="storageLocation"
+          placeholder="N/A"
+          labelPlacement="outside-top"
+          defaultValue="N/A"
+        />
         <MoneyInput subject="RUBS" />
         <MoneyInput subject="Pet Rent" />
         <MoneyInput subject="Monthly Total" />
         <MoneyInput subject="Prorated" />
-        <DateInput label="Due Date" />
+        <DateInput name="proratedRentDueDate" label="Due Date" />
       </div>
     </SubjectContainer>
   );
