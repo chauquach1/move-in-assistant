ea75da306891596b38e5ea4ccb71a7c19bf300ca
chauquach1 <chau268@gmail.com>
2025-12-05 16:26:40 -0800

    changed input fields from radio group to switch

--- components/(form)/(form-sections)/Landscaping.tsx
+++ components/(form)/(form-sections)/Landscaping.tsx
@@ -1,20 +1,44 @@
+"use client";
 import { SubjectContainer } from "@/components/(containers)/SubjectContainer";
-import {RadioGroup, Radio} from "@heroui/radio";
+import { RadioGroup, Radio } from "@heroui/radio";
+import { Switch } from "@heroui/switch";
+import React, { forwardRef, useId } from "react";
+import type { HTMLAttributes, ReactNode } from "react";
+import { Select, SelectSection, SelectItem } from "@heroui/select";
 
 export const Landscaping = () => {
-
   return (
     <SubjectContainer title="Landscaping">
       <div className="grid grid-cols-1 grid-rows-2 col-span-2 gap-4">
-        <RadioGroup name="landscapingWatering" label="Watering" defaultValue="landlord" >
-          <Radio value="landlord">Landlord</Radio>
-          <Radio value="tenant">Tenant</Radio>
-        </RadioGroup>
-        <RadioGroup name="landscapingMaintenance" label="Maintenance" defaultValue="landlord" >
-          <Radio value="landlord">Landlord</Radio>
-          <Radio value="tenant">Tenant</Radio>
-        </RadioGroup>
+        <Switch
+          name="landscapingWatering"
+          className="inline-flex flex-row-reverse gap-2"
+          defaultChecked={false}
+          thumbIcon={({ isSelected, className }) =>
+            isSelected ? (
+              <span className={className}>T</span>
+            ) : (
+              <span className={className}>L</span>
+            )
+          }
+        >
+          <p>Watering</p>
+        </Switch>
+        <Switch
+          name="landscapingMaintenance"
+          className="inline-flex flex-row-reverse gap-2"
+          defaultChecked={false}
+          thumbIcon={({ isSelected, className }) =>
+            isSelected ? (
+              <span className={className}>T</span>
+            ) : (
+              <span className={className}>L</span>
+            )
+          }
+        >
+          <p>Maintenance</p>
+        </Switch>
       </div>
     </SubjectContainer>
   );
-}
+};
