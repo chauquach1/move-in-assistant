77932d510a35a0a4ce2a8d82d988e0854341aee4
chauquach1 <chau268@gmail.com>
2025-12-14 18:40:33 -0800

    updated NavigationBar component

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -37,25 +37,9 @@ export default function NavigationBar() {
   return (
     <Navbar>
       <NavbarBrand>
-        <p className="font-bold text-inherit">ACME</p>
+        <p className="font-bold text-inherit">CAL-PROP</p>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link aria-current="page" href="#">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
+
       <NavbarContent justify="end">
         {!user ? (
           <>
