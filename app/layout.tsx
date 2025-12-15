29e11728615a19943b19237bfe6448acf6ad351d
chauquach1 <chau268@gmail.com>
2025-12-14 18:38:09 -0800

    imported NavigationBar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
-
+import React from "react";
+import NavigationBar from "@/components/NavigationBar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body
       >
         <Providers>
+          <NavigationBar />
           {children}
         </Providers>
       </body>
